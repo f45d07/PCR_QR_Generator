3c628b0f4089e0c4365145026796f49f0a991bca
f45d07 <f45d07@users.noreply.github.com>
2021-01-28 10:13:12 +0300

    Fix options

---
+++ precache-manifest.cbeed1d88eff215fb01acbe54c6d2433.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8033c3629baf0c6eeecbb58c503a9f51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8bb0ed5f40a860772a2",
+    "url": "/static/css/2.8c3a7296.chunk.css"
+  },
+  {
+    "revision": "afe07be8d4f219b13fc0",
+    "url": "/static/css/main.16109f13.chunk.css"
+  },
+  {
+    "revision": "f8bb0ed5f40a860772a2",
+    "url": "/static/js/2.2775c583.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.2775c583.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afe07be8d4f219b13fc0",
+    "url": "/static/js/main.7ccf491a.chunk.js"
+  },
+  {
+    "revision": "41fa474fea541bcef6cc",
+    "url": "/static/js/runtime-main.58d61a61.js"
+  },
+  {
+    "revision": "bd39f4dc572c3d73c11f5c88a17989e8",
+    "url": "/static/media/logo.bd39f4dc.png"
+  }
+]);
